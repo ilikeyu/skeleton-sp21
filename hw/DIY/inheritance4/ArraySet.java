79ba6701d0b188e7af79289b0211d2c6d2371b99
ilikeyu <cayeer96@gmail.com>
2021-07-25 01:29:22 +0800

    array set

--- hw/DIY/inheritance4/ArraySet.java
+++ hw/DIY/inheritance4/ArraySet.java
@@ -1,23 +1,41 @@
-public class ArraySet<T> {
+package DIY.inheritance4;
+
+public class ArraySet<T> implements Iterable<T> {
+
+    public static final int INIT_SIZE = 100;
+    private T[] item;
+    private int size;
 
     public ArraySet() {
+        item = (T[]) new Object[INIT_SIZE];
+        size = 0;
     }
 
     /* Returns true if this map contains a mapping for the specified key.
      */
     public boolean contains(T x) {
+        for (int index = 0; index < size; index++) {
+            if ( item[index].equals(x) ){
+                return true;
+            }
+        }
         return false;
     }
 
     /* Associates the specified value with the specified key in this map. 
        Throws an IllegalArgumentException if the key is null. */
     public void add(T x) {
+        if ( contains(x) ){
+            return;
+        }
+        item[size]  = x;
+        size += 1;
         return;
     }
 
     /* Returns the number of key-value mappings in this map. */
     public int size() {
-        return 0;
+        return size;
     }
 
     public static void main(String[] args) {
@@ -29,6 +47,26 @@ public class ArraySet<T> {
         s.add("fish");        
         System.out.println(s.contains("horse"));        
         System.out.println(s.size());       
+    }
+
+    @Override
+    public void IterateToString() {
+        for (int index = 0; index < size; index++) {
+            System.out.print( item[index] + " --> " );
+        }
+        System.out.println();
+        return;
+    }
+
+    @Override
+    public boolean equals(ArraySet as) {
+        if (as.size() != size)
+            return false;
+        for (int index = 0; index < size; index++) {
+            if ( !as.contains( item[index] ) )
+                return false;
+        }
+        return true;
     }
 
     /* Also to do:
---
+++ hw/DIY/inheritance4/Iterable.java
@@ -0,0 +1,8 @@
+package DIY.inheritance4;
+
+public interface Iterable<T> {
+
+    public void IterateToString();
+
+    public boolean equals(ArraySet as);
+}
